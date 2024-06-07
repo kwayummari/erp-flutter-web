e5a5327c68754b4644f87f8c5b790611344385b4
Kwayu Mmari <mmaridenis@gmail.com>
2024-06-07 16:38:35 +0300

    connecting dashboard page

--- lib/src/screens/dashboard/dashboard.dart
+++ lib/src/screens/dashboard/dashboard.dart
@@ -1,4 +1,5 @@
 import 'package:erp/src/screens/models/layout/layout.dart';
+import 'package:erp/src/utils/app_const.dart';
 import 'package:flutter/material.dart';
 
 class dashboard extends StatefulWidget {
@@ -11,6 +12,54 @@ class dashboard extends StatefulWidget {
 class _dashboardState extends State<dashboard> {
   @override
   Widget build(BuildContext context) {
-    return layout(child: Container());
+    return layout(child: Column(
+      children: [
+       Padding(
+         padding: const EdgeInsets.only(left: 100, right: 100),
+         child: Row(
+          children: [
+            Material(
+              elevation: 10,
+              color: AppConst.white,
+              child: Container(
+                width: 350.0,
+              height: 200.0,
+              decoration: BoxDecoration(
+                color: AppConst.black,
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              ),
+            ),
+            SizedBox( width: 50,),
+            Material(
+              elevation: 10,
+              color: AppConst.white,
+              child: Container(
+                width: 350.0,
+              height: 200.0,
+              decoration: BoxDecoration(
+                color: AppConst.black,
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              ),
+            ),
+            SizedBox( width: 50,),
+            Material(
+              elevation: 10,
+              color: AppConst.white,
+              child: Container(
+                width: 350.0,
+              height: 200.0,
+              decoration: BoxDecoration(
+                color: AppConst.black,
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              ),
+            )
+          ],
+         ),
+       )
+      ],
+    ));
   }
 }
