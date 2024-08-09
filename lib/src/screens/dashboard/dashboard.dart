42613155a5a789261aa5d4d4f0420faa8ada36d6
Kwayu Mmari <mmaridenis@gmail.com>
2024-08-09 17:43:49 +0300

    fixed

--- lib/src/screens/dashboard/dashboard.dart
+++ lib/src/screens/dashboard/dashboard.dart
@@ -1,5 +1,6 @@
 import 'package:erp/src/screens/models/layout/layout.dart';
 import 'package:erp/src/utils/app_const.dart';
+import 'package:erp/src/widgets/app_text.dart';
 import 'package:flutter/material.dart';
 
 class dashboard extends StatefulWidget {
@@ -12,53 +13,193 @@ class dashboard extends StatefulWidget {
 class _dashboardState extends State<dashboard> {
   @override
   Widget build(BuildContext context) {
-    return layout(child: Column(
+    return layout(
+        child: Column(
       children: [
-       Padding(
-         padding: const EdgeInsets.only(left: 100, right: 100),
-         child: Row(
-          children: [
-            Material(
-              elevation: 10,
-              color: AppConst.white,
-              child: Container(
-                width: 350.0,
-              height: 200.0,
-              decoration: BoxDecoration(
+        Padding(
+          padding: const EdgeInsets.only(left: 100, right: 100),
+          child: Row(
+            children: [
+              Material(
+                elevation: 10,
                 color: AppConst.white,
-                borderRadius: BorderRadius.circular(10.0),
+                child: Container(
+                  width: 250.0,
+                  height: 100.0,
+                  decoration: BoxDecoration(
+                    color: AppConst.white,
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      children: [
+                        Container(
+                          height: 60,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              AppText(
+                                txt: 'Total sales',
+                                size: 15,
+                                color: AppConst.grey,
+                                weight: FontWeight.bold,
+                              ),
+                              AppText(
+                                txt: 'Tsh. 1,000,000',
+                                size: 20,
+                                color: AppConst.black,
+                                weight: FontWeight.bold,
+                              ),
+                            ],
+                          ),
+                        ),
+                        Spacer(),
+                        Icon(Icons.graphic_eq, color: AppConst.grey,)
+                      ],
+                    ),
+                  ),
+                ),
               ),
+              SizedBox(
+                width: 50,
               ),
-            ),
-            SizedBox( width: 50,),
-            Material(
-              elevation: 10,
-              color: AppConst.white,
-              child: Container(
-                width: 350.0,
-              height: 200.0,
-              decoration: BoxDecoration(
+              Material(
+                elevation: 10,
                 color: AppConst.white,
-                borderRadius: BorderRadius.circular(10.0),
+                child: Container(
+                  width: 250.0,
+                  height: 100.0,
+                  decoration: BoxDecoration(
+                    color: AppConst.white,
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      children: [
+                        Container(
+                          height: 60,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              AppText(
+                                txt: 'Total purchases',
+                                size: 15,
+                                color: AppConst.grey,
+                                weight: FontWeight.bold,
+                              ),
+                              AppText(
+                                txt: 'Tsh. 200,000',
+                                size: 20,
+                                color: AppConst.black,
+                                weight: FontWeight.bold,
+                              ),
+                            ],
+                          ),
+                        ),
+                        Spacer(),
+                        Icon(Icons.money, color: AppConst.grey,)
+                      ],
+                    ),
+                  ),
+                ),
               ),
+              SizedBox(
+                width: 50,
               ),
-            ),
-            SizedBox( width: 50,),
-            Material(
-              elevation: 10,
-              color: AppConst.white,
-              child: Container(
-                width: 350.0,
-              height: 200.0,
-              decoration: BoxDecoration(
+              Material(
+                elevation: 10,
                 color: AppConst.white,
-                borderRadius: BorderRadius.circular(10.0),
+                child: Container(
+                  width: 250.0,
+                  height: 100.0,
+                  decoration: BoxDecoration(
+                    color: AppConst.white,
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      children: [
+                        Container(
+                          height: 60,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              AppText(
+                                txt: 'Total products',
+                                size: 15,
+                                color: AppConst.grey,
+                                weight: FontWeight.bold,
+                              ),
+                              AppText(
+                                txt: 'Tsh. 100',
+                                size: 20,
+                                color: AppConst.black,
+                                weight: FontWeight.bold,
+                              ),
+                            ],
+                          ),
+                        ),
+                        Spacer(),
+                        Icon(Icons.propane_tank_rounded, color: AppConst.grey,)
+                      ],
+                    ),
+                  ),
+                ),
               ),
+              SizedBox(
+                width: 50,
               ),
-            )
-          ],
-         ),
-       )
+              Material(
+                elevation: 10,
+                color: AppConst.white,
+                child: Container(
+                  width: 250.0,
+                  height: 100.0,
+                  decoration: BoxDecoration(
+                    color: AppConst.white,
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      children: [
+                        Container(
+                          height: 60,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              AppText(
+                                txt: 'Net profit',
+                                size: 15,
+                                color: AppConst.grey,
+                                weight: FontWeight.bold,
+                              ),
+                              AppText(
+                                txt: 'Tsh. 800,000',
+                                size: 20,
+                                color: AppConst.black,
+                                weight: FontWeight.bold,
+                              ),
+                            ],
+                          ),
+                        ),
+                        Spacer(),
+                        Icon(Icons.money, color: AppConst.grey,)
+                      ],
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        )
       ],
     ));
   }
