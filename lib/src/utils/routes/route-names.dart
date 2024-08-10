ea22bbe386c07dfe3905164c161bcbc125bcf16e
Kwayu Mmari <mmaridenis@gmail.com>
2024-08-11 02:38:35 +0300

    fixed

--- lib/src/utils/routes/route-names.dart
+++ lib/src/utils/routes/route-names.dart
@@ -1,19 +1,19 @@
 class RouteNames {
-  static const String home = '/home';
-  static const String login = '/login';
-  static const String registration = '/registration';
-  static const String splash = '/splash';
-  static const String dashboard = '/dashboard';
-  static const String userManagement = '/user';
-  static const String inventory = '/inventory';
-  static const String supplier = '/supplier';
-  static const String roles = '/roles';
-  static const String permissions = '/permissions';
-  static const String branch = '/branch';
-  static const String purchaseOrder = '/purchaseOrder';
-  static const String grn = '/grn';
-  static const String completedGrn = '/completeGrn';
-  static const String saleManagement = '/sellProduct';
-  static const String todayReport = '/todayReport';
-  static const String reportRange = '/reportRange';
+  static const String home = '/Home';
+  static const String login = '/Login';
+  static const String registration = '/Registration';
+  static const String splash = '/Splash';
+  static const String dashboard = '/Dashboard';
+  static const String userManagement = '/User';
+  static const String inventory = '/Inventory';
+  static const String supplier = '/Supplier';
+  static const String roles = '/Roles';
+  static const String permissions = '/Permissions';
+  static const String branch = '/Branch';
+  static const String purchaseOrder = '/PurchaseOrder';
+  static const String grn = '/Grn';
+  static const String completedGrn = '/CompleteGrn';
+  static const String saleManagement = '/SellProduct';
+  static const String todayReport = '/TodayReport';
+  static const String reportRange = '/ReportRange';
 }
