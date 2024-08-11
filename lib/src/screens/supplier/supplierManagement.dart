5ace66a959873023bfe40e9f1979cd6a1d1e5dab
Kwayu Mmari <mmaridenis@gmail.com>
2024-08-11 03:29:03 +0300

    fixed

--- lib/src/screens/supplier/supplierManagement.dart
+++ lib/src/screens/supplier/supplierManagement.dart
@@ -2,6 +2,8 @@ import 'package:erp/src/gateway/supplierService.dart';
 import 'package:erp/src/provider/rowProvider.dart';
 import 'package:erp/src/screens/supplier/addSupplier.dart';
 import 'package:erp/src/screens/supplier/editSupplierForm.dart';
+import 'package:erp/src/utils/auth_utils.dart';
+import 'package:erp/src/utils/routes/route-names.dart';
 import 'package:erp/src/widgets/app_text.dart';
 import 'package:flutter/material.dart';
 import 'package:erp/src/screens/models/layout/layout.dart';
@@ -10,6 +12,7 @@ import 'package:erp/src/widgets/app_button.dart';
 import 'package:erp/src/widgets/app_modal.dart';
 import 'package:erp/src/widgets/app_table.dart';
 import 'package:erp/src/widgets/app_tabular_widget.dart';
+import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 
 class supplierManagement extends StatefulWidget {
@@ -61,7 +64,16 @@ class _supplierManagementState extends State<supplierManagement> {
   @override
   void initState() {
     super.initState();
+    _checkLoginStatus();
     fetchData();
+  }
+
+  Future<void> _checkLoginStatus() async {
+    if (!await isUserLoggedIn()) {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        context.go(RouteNames.login);
+      });
+    }
   }
 
   final List<String> titles = ['Phone', 'Name', 'Tin', 'Vrn', 'Location'];
