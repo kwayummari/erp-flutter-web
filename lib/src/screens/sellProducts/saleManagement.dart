f561b45af39c149ab7cf7b86db22e8705ba5d75d
Kwayu Mmari <mmaridenis@gmail.com>
2024-10-28 16:35:50 +0300

    fixed data

--- lib/src/screens/sellProducts/saleManagement.dart
+++ lib/src/screens/sellProducts/saleManagement.dart
@@ -1,18 +1,15 @@
-import 'dart:math';
-import 'package:erp/src/gateway/salesProductServices.dart';
-import 'package:erp/src/screens/sellProducts/topOfSales.dart';
+import 'package:erp/src/functions/splash.dart';
+import 'package:erp/src/gateway/inventoryService.dart';
+import 'package:erp/src/screens/roles/addRoles.dart';
+import 'package:erp/src/utils/app_const.dart';
 import 'package:erp/src/utils/auth_utils.dart';
 import 'package:erp/src/utils/routes/route-names.dart';
-import 'package:erp/src/widgets/app-offlineDropdownFormField.dart';
-import 'package:erp/src/widgets/app_table4.dart';
-import 'package:erp/src/gateway/purchaseOrderService.dart';
-import 'package:erp/src/utils/app_const.dart';
-import 'package:erp/src/widgets/app_button.dart';
-import 'package:erp/src/widgets/app_text.dart';
+import 'package:erp/src/widgets/app_modal.dart';
 import 'package:flutter/material.dart';
+import 'package:erp/src/gateway/rolesService.dart';
+import 'package:erp/src/widgets/app_text.dart';
 import 'package:erp/src/screens/models/layout/layout.dart';
 import 'package:go_router/go_router.dart';
-import 'package:intl/intl.dart';
 
 class SaleManagement extends StatefulWidget {
   const SaleManagement({super.key});
@@ -22,63 +19,26 @@ class SaleManagement extends StatefulWidget {
 }
 
 class _SaleManagementState extends State<SaleManagement> {
-  List<Map<String, dynamic>> salesData = [];
+  List productData = [];
   bool isLoading = true;
   bool hasError = false;
-  var purchaseId;
-  var orderId;
-  var supplierId;
-  var todayDate;
-  var randomNumber;
-  bool fetchSupplier = false;
-  double totalAmount = 0.0;
-  double vatAmount = 0.0;
-  double grandTotal = 0.0;
-  final List<String> titles = [
-    'Name',
-    'Description',
-    'Quantity',
-    'Price',
-    'Total',
-  ];
-  GlobalKey _printKey = GlobalKey();
-
-  void refreshSuppliers() {
-    setState(() {
-      fetchSupplier = !fetchSupplier;
-    });
-  }
 
   Future<void> fetchData() async {
     try {
-      salesProductServices productListServices = salesProductServices();
-      final orderListResponse = await productListServices.getSalesServices(
-          context, randomNumber.toString());
-      if (orderListResponse != null && orderListResponse['products'] != null) {
-        salesData = [];
-        totalAmount = 0.0;
+      SplashFunction splashDetails = SplashFunction();
+      final fetchingBranchId = await splashDetails.getFetchingBranchId();
+      final fetchingBranchName = await splashDetails.getFetchingBranchName();
+
+      rolesServices rolesService = rolesServices();
+      final rolesResponse = await rolesService.getRoles(context);
+
+      inventoryServices inventoryService = inventoryServices();
+      final productResponse =
+          await inventoryService.getProduct(context, fetchingBranchId);
+      print(productResponse);
+      if (productResponse != null && productResponse['products'] != null) {
         setState(() {
-          salesData = [];
-          for (var inventory in orderListResponse['products']) {
-            double total = double.parse(inventory['buyingPrice']) *
-                double.parse(inventory['quantity']);
-            totalAmount += total;
-            salesData.add({
-              'id': inventory['id'].toString(),
-              'mainId': inventory['mainId'].toString(),
-              'orderedId': inventory['orderedId'].toString(),
-              'name': inventory['name'].toString(),
-              'description': inventory['description'].toString(),
-              'quantity': inventory['quantity'].toString(),
-              'price': inventory['buyingPrice'].toString(),
-              'total': total.toString(),
-            });
-          }
-          vatAmount = totalAmount * 0.2;
-          grandTotal = totalAmount + vatAmount;
-          isLoading = false;
-        });
-        setState(() {
+          productData = productResponse['products'];
           isLoading = false;
         });
       } else {
@@ -88,41 +48,19 @@ class _SaleManagementState extends State<SaleManagement> {
         });
       }
     } catch (e) {
+      print('Error fetching data: $e');
       setState(() {
         hasError = true;
         isLoading = false;
       });
     }
   }
-
-  Future<void> saveData(randomNumber, method) async {
-    try {
-      purchaseOrderServices purchaseOrderService = purchaseOrderServices();
-      final purchaseOrderResponse = await purchaseOrderService.saveSalesOrder(
-          context, randomNumber, method);
-    } catch (e) {
-      setState(() {
-        hasError = true;
-        isLoading = false;
-      });
-    }
-  }
-
-  String selectedOption = 'Payment by cash';
-  List<String> options = [
-    'Payment by cash',
-    'Payment by credit',
-  ];
 
   @override
   void initState() {
     super.initState();
     _checkLoginStatus();
-    var now = DateTime.now();
-    var formatter = DateFormat('yyyy-MM-dd');
-    todayDate = formatter.format(now);
-    Random random = Random();
-    randomNumber = random.nextInt(1000000);
+    fetchData();
   }
 
   Future<void> _checkLoginStatus() async {
@@ -137,166 +75,111 @@ class _SaleManagementState extends State<SaleManagement> {
   Widget build(BuildContext context) {
     return layout(
       child: SingleChildScrollView(
-        child: Align(
-          alignment: Alignment.center,
-          child: RepaintBoundary(
-            key: _printKey,
-            child: Container(
-              width: MediaQuery.of(context).size.width - 400,
-              height: MediaQuery.of(context).size.height - 100,
-              decoration: BoxDecoration(
-                shape: BoxShape.rectangle,
-                color: AppConst.grey100,
-                borderRadius: BorderRadius.circular(10.0),
-                border: Border.all(color: Colors.black),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black,
-                    offset: Offset(4, 4),
-                    blurRadius: 10,
-                  ),
-                ],
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  TopOfSales(
-                    randomNumber: randomNumber,
-                    todayDate: todayDate,
-                    refreshSuppliers: refreshSuppliers,
-                    fetchSupplier: fetchSupplier,
-                    supplierId: supplierId,
-                    fetchData: fetchData,
-                    fetchData1: fetchData,
-                    orderId: orderId,
-                    onSupplierChanged: (value) {
-                      setState(() {
-                        supplierId = value;
-                        fetchData();
-                      });
-                    },
-                    purchaseData: salesData,
-                  ),
-                  Container(
-                    height: 250,
-                    width: MediaQuery.of(context).size.width,
-                    child: ReusableTable4(
-                      fetchData1: fetchData,
-                      orderId: orderId,
-                      supplierId: supplierId,
-                      deleteModalHeight: 300,
-                      deleteModalWidth: 500,
-                      editModalHeight: 550,
-                      editModalWidth: 500,
-                      fetchData: fetchData,
-                      columnSpacing: 100,
-                      titles: titles,
-                      randomNumber: randomNumber.toString(),
-                      cellBuilder: (context, row, title) {
-                        return Text(
-                            row[title.toLowerCase().replaceAll(' ', '')] ?? '');
-                      },
-                      onClose: fetchData,
-                      url: 'delete_product_sale',
-                      data: salesData,
-                    ),
-                  ),
-                  Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Container(
-                          width: 230,
-                          child: AppDropdownTextFormField(
-                            labelText: 'Select Payment Method',
-                            options: options,
-                            value: selectedOption,
-                            onChanged: (newValue) {
-                              setState(() {
-                                selectedOption = newValue!;
-                              });
-                            },
+        child: Column(
+          children: [
+            if (isLoading)
+              Center(child: CircularProgressIndicator())
+            else if (hasError)
+              Center(child: Text('')),
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 4,
+                  mainAxisSpacing: 8.0,
+                  crossAxisSpacing: 8.0,
+                  mainAxisExtent: 120,
+                ),
+                padding: EdgeInsets.all(15.0),
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemCount: productData.length + 1,
+                itemBuilder: (context, index) {
+                  if (index == productData.length) {
+                    return MouseRegion(
+                      cursor: SystemMouseCursors.click,
+                      child: GestureDetector(
+                        onTap: () {
+                          ReusableModal.show(
+                            width: 500,
+                            height: 300,
+                            context,
+                            AppText(
+                                txt: 'Add Role',
+                                size: 22,
+                                weight: FontWeight.bold),
+                            onClose: fetchData,
+                            Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: <Widget>[
+                                addRolesForm(fetchData: fetchData)
+                              ],
+                            ),
+                          );
+                        },
+                        child: Material(
+                          elevation: 10,
+                          color: Colors.grey,
+                          borderRadius: BorderRadius.circular(10.0),
+                          child: Container(
+                            width: 200, // Fixed width for each grid item
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                children: [
+                                  Icon(
+                                    Icons.add,
+                                    color: AppConst.white,
+                                  ),
+                                  SizedBox(
+                                    width: 20,
+                                  ),
+                                  AppText(
+                                    txt: ' Add Role',
+                                    size: 20,
+                                    color: Colors.white,
+                                    weight: FontWeight.bold,
+                                  ),
+                                ],
+                              ),
+                            ),
                           ),
                         ),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.only(
-                            left: 8, right: 8, bottom: 20),
+                    );
+                  } else {
+                    // Normal grid item
+                    return GestureDetector(
+                      onTap: () {},
+                      child: Material(
+                        elevation: 10,
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10.0),
                         child: Container(
-                          height: 50,
-                          child: AppButton(
-                              solidColor: Colors.green,
-                              onPress: () async {
-                                // await saveData(randomNumber.toString(),
-                                //     selectedOption.toString());
-                                // setState(() {
-                                //   supplierId = null;
-                                //   salesData = [];
-                                // });
-                                context.go(
-                                  RouteNames.printingPage,
-                                  extra: {
-                                    'company': 'JamSolutions',
-                                    'amount': '10000',
-                                    'recipientName': 'Andrew Msilu',
-                                    'senderName': 'Brian Sisti',
-                                    'phoneNumber': '0762996305',
-                                  },
-                                );
-                              },
-                              label: 'Submit and Print',
-                              borderRadius: 20,
-                              textColor: AppConst.white),
+                          width: 200,
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: AppText(
+                                  txt: productData[index]['name'] +
+                                      '(${productData[index]['sold'].toString()})',
+                                  align: TextAlign.center,
+                                  size: 18,
+                                  color: Colors.black,
+                                  weight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
-                      Spacer(),
-                      Column(
-                        children: [
-                          Align(
-                            alignment: Alignment.centerRight,
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: AppText(
-                                txt:
-                                    'Total: Tsh.${totalAmount.toStringAsFixed(2)}',
-                                size: 15,
-                                weight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                          Align(
-                            alignment: Alignment.centerRight,
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: AppText(
-                                txt:
-                                    'VAT (20%): Tsh.${vatAmount.toStringAsFixed(2)}',
-                                size: 15,
-                                weight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                          Align(
-                            alignment: Alignment.centerRight,
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: AppText(
-                                txt:
-                                    'Grand Total: Tsh.${grandTotal.toStringAsFixed(2)}',
-                                size: 15,
-                                weight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                        ],
-                      )
-                    ],
-                  )
-                ],
+                    );
+                  }
+                },
               ),
             ),
-          ),
+          ],
         ),
       ),
     );
