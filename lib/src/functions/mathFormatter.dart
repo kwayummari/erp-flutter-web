76111e599af627b4dd8adfd83a3f42997635b51d
Kwayu Mmari <mmaridenis@gmail.com>
2024-10-29 03:01:36 +0300

    fixed data

--- lib/src/functions/mathFormatter.dart
+++ lib/src/functions/mathFormatter.dart
@@ -1,5 +1,5 @@
 class MathFormatter {
-  Future subtraction(int input1, int input2) async {
+  String subtraction(int input1, int input2) {
     final answer = input1 - input2;
     return answer.toString();
   }
